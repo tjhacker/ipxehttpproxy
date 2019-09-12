bafe8e224c40064606f57b1e84365069588b9254
tjhacker <tjhacker@users.noreply.github.com>
2019-09-12 11:14:04 -0400

    Create foo.php

---
+++ foo.php
@@ -0,0 +1,12 @@
+     <$?php
+        header ("Content-type: text/plan");
+        echo "#!ipxe\n";
+        $proxy = "http://juno.load:3128";
+        echo "set http-proxy " . $proxy . "\n";
+        echo "imgfree\n";
+        #echo "dhcp\n";
+        echo "kernel http://ftp.ussg.iu.edu/linux/centos/7/os/x86_64/images/pxeboot/vmlinuz proxy=" . $proxy . " repo=http://ftp.ussg.iu.edu/linux/centos/7/os/x86_64 text rootpw --cleartext 123cithpc time zone US/Eastern install autopart\n";
+        ecoh "initrd http://ftp.ussg.iu.edu/linux/centos/7/os/x86_64/images/pxeboot/initrd.img\n";
+        echo "boot\n";
+        ?>
+        EOF
