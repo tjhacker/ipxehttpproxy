3fc7d59ced796f84f3b30e43bf6a297c2c4bf5d6
tjhacker <tjhacker@users.noreply.github.com>
2019-09-16 16:05:34 -0400

    Update foo.php

--- foo.php
+++ foo.php
@@ -6,8 +6,8 @@ echo "set http-proxy " . $proxy . "\n";
 echo "imgfree\n";
 #echo "dhcp\n";
 #echo "kernel http://ftp.ussg.iu.edu/linux/centos/7/os/x86_64/images/pxeboot/vmlinuz proxy=" . $proxy . " repo=http://ftp.ussg.iu.edu/linux/centos/7/os/x86_64 text rootpw --cleartext 123cithpc time zone US/Eastern install autopart\n";
-echo "initrd http://ftp.ussg.iu.edu/linux/centos/7/os/x86_64/images/pxeboot/initrd.img\n";
 echo "kernel http://ftp.ussg.iu.edu/linux/centos/7/os/x86_64/images/pxeboot/vmlinuz proxy=" . $proxy . " repo=http://ftp.ussg.iu.edu/linux/centos/7/os/x86_64 text ks=http://juno.load/ks.cfg\n";
+echo "initrd http://ftp.ussg.iu.edu/linux/centos/7/os/x86_64/images/pxeboot/initrd.img\n";
 #echo "vmlinuz initrd=initrd.img ks=http://juno.load/ks.cfg\n";
 echo "boot\n";
 ?>
