5cbc78bf238d0bddb3fedf465560ed4429456182
tjhacker <tjhacker@users.noreply.github.com>
2021-03-24 16:25:38 -0400

    Update foo.php

--- foo.php
+++ foo.php
@@ -39,7 +39,8 @@ echo $post_cmd;
 echo "imgfree dhcp_opt.php\n";
 echo "dhcp\n";
 echo "sleep 4\n";
-echo "imgfetch --timeout 1000000 nfs://192.7.7.4/freenas/boot/pxeboot\n";
+echo ":retry_freeenas_nfs\n";
+echo "imgfetch --timeout 1000000 nfs://192.7.7.4/freenas/boot/pxeboot || goto retry_freenas_nfs\n";
 echo "echo Image loaded.\n";
 echo "boot pxeboot\n";
 ?>
